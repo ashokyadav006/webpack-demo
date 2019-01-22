7dcc0362f94f176de930c552c1e8977224b2378a
Ashok Yadav <yadaas@amazon.com>
2019-01-22 22:19:49 +0530

    Add code spliting

--- src/component.js
+++ src/component.js
@@ -6,5 +6,15 @@ export default (text = "Hello India") => {
     element.innerHTML = `<img src="${logo}" height="20px"></img> `+text;
     element.className = "pure-button";
 
+    element.onclick = () => {
+        import('./lazy')
+            .then(lazy => {
+                element.textContent = lazy.default;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
     return element;
 };
